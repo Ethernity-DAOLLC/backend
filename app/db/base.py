21ebd26150d4864f65eb4569a5a423df9282bba5
cristianftab <ethernitydao@gmail.com>
2025-12-06 11:42:43 -0300

    smtp

--- app/db/base.py
+++ app/db/base.py
@@ -1,7 +1,7 @@
 from app.db.base_class import Base
 
 from app.models.contact import Contact
-from app.models.users import User
+from app.models.users import User, Auth
 
 # from app.models.contribution import Contribution
 # from app.models.withdrawal import Withdrawal
