82807632c8f9c80d2e821d3320cd242e3a6aa92a
cristianftab <ethernitydao@gmail.com>
2026-01-07 21:10:49 -0300

    fix

--- app/api/v1/endpoints/__init__.py
+++ app/api/v1/endpoints/__init__.py
@@ -1,14 +1,13 @@
 from .auth import router as auth
 from .contact import router as contact
 from .users import router as users
-
-# Si se añaden más endpoints (ej: contributions.py, health.py, etc.)
-# from .contributions import router as contributions
-# from .health import router as health
+from .survey import router as survey 
+from .stats import router as stats 
 
 __all__ = [
     "auth",
     "contact",
     "users",
-    # "others",
+    "survey", 
+    "stats",  
 ]
