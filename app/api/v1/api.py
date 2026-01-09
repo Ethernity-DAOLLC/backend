c334eb1dbaa5157cbaf6c616f309ba4029155ac1
cristianftab <ethernitydao@gmail.com>
2026-01-09 00:33:09 -0300

    api

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter
-
 from app.api.v1.endpoints.contact import router as contact_router
 from app.api.v1.endpoints.users import router as users_router
 from app.api.v1.endpoints.auth import router as auth_router
@@ -14,33 +13,28 @@ async def health_check():
         "status": "ok",
         "message": "API v1 funcionando correctamente"
     }
-
 api_router.include_router(
     contact_router,
     prefix="/contact",
     tags=["contact"]
 )
-
 api_router.include_router(
     users_router,
     prefix="/users",
     tags=["users"]
 )
-
 api_router.include_router(
     auth_router,
     prefix="/auth",
     tags=["authentication"]
 )
-
 api_router.include_router(
     stats_router,
     prefix="/stats",
     tags=["admin-stats"]
 )
-
 api_router.include_router(
     survey_router,
-    prefix="/survey",
+    prefix="/surveys",
     tags=["survey"]
 )
